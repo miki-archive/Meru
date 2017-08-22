ea0b2309f74c0b12d88471d472d8ed7fc93be605
Mike <veldsinkie.mv@gmail.com>
2017-08-22 04:51:01 +0200

    Add: Private command process instances

---
+++ Meru.Commands/PrivateCommandProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meru.Commands
+{
+    class PrivateCommandProcessor : CommandProcessor
+    {
+        private PrivateCommandProcessorConfiguration config;
+
+        public PrivateCommandProcessor(object ownerId, PrivateCommandProcessorConfiguration config) : base(config)
+        {
+            this.config = config;
+        }
+
+
+    }
+}
---
+++ Meru.Commands/PrivateCommandProcessorConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meru.Commands
+{
+    class PrivateCommandProcessorConfiguration : CommandProcessorConfiguration
+    {
+        public TimeSpan ExpirationSpan = new TimeSpan(0);
+        public bool Expires = false;
+    }
+}
